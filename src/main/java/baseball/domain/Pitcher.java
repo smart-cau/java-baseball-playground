a0507eec6cccc0e5128fdca778302cfbd0997b38
Jung Hyun Woo <1995hyunwoo@naver.com>
2023-05-05 13:43:32 +0000

    feat (baseball) random Numer 생성하는 Pitcher class & test 생성

--- src/main/java/baseball/domain/Pitcher.java
+++ src/main/java/baseball/domain/Pitcher.java
@@ -1,5 +1,22 @@
 package baseball.domain;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Pitcher {
+
+    private Set<Integer> numbers;
+
+    Pitcher() {
+        numbers = new HashSet<Integer>();
+    }
+
+    public Set<Integer> pitch() {
+        while (0 <= numbers.size() && numbers.size() < 3) {
+            int rand = (int)(Math.random() * 9) + 1;
+            numbers.add(rand);
+        }
+        return numbers;
+    }
     
 }
--- src/test/java/baseball/domain/PitcherTest.java
+++ src/test/java/baseball/domain/PitcherTest.java
@@ -1,5 +1,28 @@
 package baseball.domain;
 
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
 public class PitcherTest {
-    
+    Pitcher pitcher;
+
+    @BeforeEach
+    void setup () {
+        pitcher = new Pitcher();
+    }
+
+    @Test
+    void pitchSuccess() {
+        Set<Integer> pitch = pitcher.pitch();
+
+        assertThat(pitch.size()).isEqualTo(3);
+
+        pitch.forEach((num) -> {
+            assertThat(num).isBetween(1, 9);
+        });
+    }
 }
